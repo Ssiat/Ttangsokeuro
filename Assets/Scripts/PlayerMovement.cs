ad83d3fd927d90ff970eebf29639ea56611c3735
Pdom <ahb0327@naver.com>
2016-10-27 18:25:18 +0900

    [추가] PlayerMovement.cs
    파일명 변경 예정.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public int speed = 5;
+
+	void Start ()
+	{
+	
+	}
+	
+	void Update ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		float moveVertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+		transform.position = new Vector3(transform.position.x + moveHorizontal, transform.position.y + moveVertical, 0f);
+	}
+}
