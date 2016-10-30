87c93aae70233b361671fb00aab5563bcd1c0220
Pdom <ahb0327@naver.com>
2016-10-30 13:38:12 +0900

    [변경] PixelPerfectSprite.cs

--- Assets/Scripts/PixelPerfect/PixelPerfectSprite.cs
+++ Assets/Scripts/PixelPerfect/PixelPerfectSprite.cs
@@ -3,11 +3,48 @@ using System.Collections;
 
 namespace Ssiat.PixelPerfect
 {
-	public class PixelPerfectSprite : PixelPerfectObject
+	[ExecuteInEditMode]
+	public class PixelPerfectSprite : MonoBehaviour
 	{
-		new void LateUpdate()
+		public int scale = 1;
+		public bool positionForScale = true;
+
+		private GameObject _sprite;
+
+		void OnEnable()
 		{
-			base.LateUpdate();
+			_sprite = new GameObject("Sprite");
+			_sprite.transform.parent = transform;
+			_sprite.AddComponent<SpriteRenderer>().sprite = GetComponent<SpriteRenderer>().sprite;
+			GetComponent<SpriteRenderer>().sprite = null;
+		}
+
+		protected void LateUpdate()
+		{
+			if (transform.hasChanged)
+				SetPixelPerfect();
+		}
+
+		protected void SetPixelPerfect()
+		{
+			SetPixelPerfectPosition();
+			SetPixelPerfectScale();
+
+			transform.hasChanged = false;
+		}
+
+		protected void SetPixelPerfectPosition()
+		{
+			if (positionForScale)
+				_sprite.transform.position = new Vector2((Mathf.Round(transform.position.x / scale) * scale), (Mathf.Round(transform.position.y / scale) * scale));
+			else
+				_sprite.transform.position = new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+		}
+
+		protected void SetPixelPerfectScale()
+		{
+			float pixelsPerUnit = GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
+			transform.localScale = new Vector2(scale * pixelsPerUnit, scale * pixelsPerUnit);
 		}
 	}
 }
