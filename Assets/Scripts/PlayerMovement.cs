3e39be1f47ccf329293c32229bb36ead751bf1b0
Pdom <ahb0327@naver.com>
2017-02-04 20:28:00 +0900

    [변경] PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,23 @@ public class PlayerMovement : MonoBehaviour
 {
 	public int speed = 5;
 
-	void Start ()
+	private Rigidbody2D _rigidbody2D;
+
+	void Start()
 	{
-	
+		_rigidbody2D = GetComponent<Rigidbody2D>();
 	}
 	
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0f, 0f);
+
+		_rigidbody2D.AddForce(movement * speed);
+	}
+
+	/*
 	void Update ()
 	{
 		float moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
@@ -17,4 +29,5 @@ public class PlayerMovement : MonoBehaviour
 
 		transform.position = new Vector3(transform.position.x + moveHorizontal, transform.position.y + moveVertical, 0f);
 	}
+	*/
 }
